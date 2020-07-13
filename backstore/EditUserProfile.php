b5c4faec9614c0613763e6e45980e43357292797
Felix Beaulieu <felix.beaulieu80@gmail.com>
2020-07-13 19:28:52 -0400

    WIP

---
+++ backstore/EditUserProfile.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Edit User Profile</title>
+    <link rel="stylesheet" type="text/css" href="../css/backstore.css"/>
+</head>
+<header>
+    <img src="../images/logo.png" alt="Green Mart" height="78"/>
+    <div class="topnav">
+        <a href="#ProductList">Product List</a>
+        <a class="active" href="#UserList">User List</a>
+        <a href="#OrderList">Order List</a>
+    </div>
+</header>
+<body>
+<h1>Edit User Profile</h1>
+<form>
+    <th></th>
+    <tr></tr>
+    <label>Username:</label>
+    <label>Password:</label>
+    <hline/>
+    <label>Address:</label>
+</form>
+</body>
+<footer><a href="UserList.php">&lt;&lt;Back</a> </footer>
+</html>
---
+++ backstore/UserList.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <title>User List</title>
+    <link rel="stylesheet" type="text/css" href="../css/backstore.css"/>
+</head>
+<header>
+    <img src="../images/logo.png" alt="Green Mart" height="78"/>
+    <div class="topnav" id="gm-topnav">
+        <a href="#ProductList">Product List</a>
+        <a class="active" href="#UserList">User List</a>
+        <a href="#OrderList">Order List</a>
+    </div>
+</header>
+<body>
+<div id="ProductList">
+    <p>Product List Placeholder</p>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
+<div id="UserList">
+    <h1>User List</h1>
+    <p>User List Page</p>
+    <ul>
+        <li><a href="EditUserProfile.php">User1</a></li>
+    </ul>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
+<div id="OrderList">
+    <p>Order List Placeholder</p>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
+</body>
+<footer><p style="color: lightgray">Created my free logo at LogoMakr.com</p></footer>
+</html>
