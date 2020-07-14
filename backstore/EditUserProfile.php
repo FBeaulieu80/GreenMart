1c52792a23da0821c29eee75c733593045508b48
Felix Beaulieu <felix.beaulieu80@gmail.com>
2020-07-14 15:16:16 -0400

    WIP

--- backstore/EditUserProfile.php
+++ backstore/EditUserProfile.php
@@ -3,16 +3,41 @@
 <head>
     <title>Edit User Profile</title>
     <link rel="stylesheet" type="text/css" href="../css/backstore.css"/>
+    <link rel="stylesheet" type="text/css" href="../css/p1.css"/>
 </head>
 <header>
-    <img src="../images/logo.png" alt="Green Mart" height="78"/>
+    <!-- Logo and Search bar-->
     <div class="topnav">
-        <a href="#ProductList">Product List</a>
-        <a class="active" href="#UserList">User List</a>
-        <a href="#OrderList">Order List</a>
+        <ul>
+            <li><img src="../images/logo_new.png" alt="Green Mart logo" id="logo"></li>
+            <li style="float:right">
+                <div class="search-bar">
+                    <form actio="/action_page.php">
+                        <input type="text" placeholder="Search for a product" name="search">
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <!-- Main navigation bar with Aisles, random buttons, login, sign up, checkout-->
+    <div class="mainnav">
+        <ul>
+            <li><a href="aisles/p2.html">Aisles</a></li> <!-- TODO: Implement dropdown menu -->
+            <li><a href="deals/">Deals</a></li>
+            <li><a href="recipes/">Recipes</a></li>
+            <li><a href="blog/">Blog</a></li>
+
+            <li style="float:right"><a href="cart/">Shopping Cart (0)</a></li>
+            <li style="float:right"><a href="signup/">Sign Up</a></li>
+            <li style="float:right"><a href="login/">Log In</a></li>
+
+
+        </ul>
     </div>
 </header>
-<body>
+<body class="main">
 <h1>Edit User Profile</h1>
 <form>
     <th></th>
---
+++ backstore/index.php
@@ -0,0 +1,50 @@
+<html lang="en" xmlns:style="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Green Mart!</title>
+    <link rel="stylesheet" type="text/css" href="../css/backstore.css" />
+    <meta name="author" content="Felix Beaulieu">
+</head>
+
+<body>
+<!-- Logo and Search bar-->
+<div class="topnav">
+    <ul>
+        <li><img src="../images/logo_new.png" alt="Green Mart logo" id="logo"></li>
+        <!--li style="float:right">
+            <div class="search-bar">
+                <form actio="/action_page.php">
+                    <input type="text" placeholder="Search for a product" name="search">
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </li-->
+    </ul>
+</div>
+
+<!-- Main navigation bar with Aisles, random buttons, login, sign up, checkout-->
+<div class="mainnav">
+    <ul>
+        <li><a href="404.php">Inventory</a></li>
+        <li><a href="UserList.php">Users</a></li>
+        <li><a href="404.php">Orders</a></li>
+
+        <li style="float:right"><a href="login/">Log In</a></li>
+    </ul>
+</div>
+
+<div class="main">
+
+
+</div>
+</body>
+
+<footer>
+    <ul>
+        <li>Copyright &copy 2020 Green Mart Inc. All Rights Reserved.</li>
+        <li>Created my free logo at <a href="https://logomakr.com/">Logomakr.com</a></li>
+
+    </ul>
+</footer>
+
+</html>
