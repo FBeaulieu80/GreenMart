5fe3314ea8bc92fa1e968fa2b0e4e996eed9e70e
Laura Boivin <laurasuzanneboivin@gmail.com>
2020-07-15 19:21:05 -0400

    More or less satisfied with the hmtl

--- backstore/p11.php
+++ backstore/p11.php
@@ -7,6 +7,7 @@
 <?php require "common/header.php"; ?>
 <h1>Order List</h1>
 <button>Add</button>
+<button>Edit</button>
 
 <table marginwidth="100%">
     <tr>
@@ -16,13 +17,46 @@
         <th>Seller</th>
         <th>Order Date</th>
         <th>Arrived</th>
-        <th>Date Arrived</th>
-
-
-
-
         <th></th>
+
     </tr>
+    <tr>
+        <td>Organic Banana</td>
+        <td>4HKZ19</td>
+        <td><weight>30lbs</weight></td>
+        <td>JohannesGardenLte</td>
+        <td><date>2020-07-12</date></td>
+        <td><form action = ""><select name = "answer"><option>No</option><option>Yes</option></form></td>
+        <th><button>Delete</button></th>
+    </tr>
+    <tr>
+        <td>Yummy Chickpeas</td>
+        <td>7ZYT28</td>
+        <td>50</td>
+        <td>PeaHavenCo</td>
+        <td><date>2020-07-10</date></td>
+        <td><form action = ""><select name = "answer"><option>No</option><option>Yes</option></form></td>
+        <th><button>Delete</button></th>
+    </tr>
+    <tr>
+        <td>Cherry Tomatoes</td>
+        <td>96YRE4P</td>
+        <td>25lbs</td>
+        <td>JerrysDream</td>
+        <td><date>2020-07-09</date></td>
+        <td><form action = ""><select name = "answer"><option>No</option><option>Yes</option></form></td>
+        <th><button>Delete</button></th>
+    </tr>
+    <tr>
+        <td>Jimmy's Natural Potato Chips</td>
+        <td>46RY3X</td>
+        <td>30</td>
+        <td>JimmysBasement</td>
+        <td><date>2020-07-05</date></td>
+        <td><form action = ""><select name = "answer"><option>No</option><option>Yes</option></form></td>
+        <th><button>Delete</button></th>
+    </tr>
+
 </table>
 <?php require "common/footer.html"; ?>
 </body>
