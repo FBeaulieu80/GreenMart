7645dcf762cbdfdb565b7405f37364fa5df780ce
Laura Boivin <laurasuzanneboivin@gmail.com>
2020-07-16 15:16:01 -0400

    Worked with the css for this page.

--- backstore/p12.php
+++ backstore/p12.php
@@ -9,54 +9,60 @@
 
 <?php require "common/header.php"; ?>
 <body widthmargin = "100%">
-<h1 align = "center">Edit Order List<br/>
-<button>Save</button></h1>
+<h1 align = "center">Edit Order List<br/></h1>
 
-    <form action = "">
-    Product Name:
-    <textarea name = "Product Name" rows = "1" cols="20"></textarea>
-    </form>
+<form action = "" >
+    <select name = "editProduct">
+        <option>New Product</option>
+        <option>Organic Banana</option>
+        <option>Yummy Chickpeas</option>
+        <option>Cherry Tomatoes</option>
+        <option>Jimmy's Natural Potato Chips</option>
+    </select>
+</form>
 
-    <form action = "">
-            Product ID:
-            <textarea name = "Product ID" rows = "1" cols="20"></textarea>
-    </form>
+<div class = editOrder>
+    <table marginwidth="100%" cellspacing = "20" align = "center">
+        <tr>
+            <th>Product Name:</th>
+            <td><form action = "">
+                    <textarea name = "Product Name" rows = "1" cols="20"></textarea>
+                </form></td>
+        </tr>
+        <tr>
+            <th>Product ID:</th>
+            <td><form action = "">
+                    <textarea name = "Product ID" rows = "1" cols="20"></textarea>
+                </form></td>
+        </tr>
+        <tr>
+            <th>Units / LBS:</th>
+            <td><form action = "">
+                    <textarea name = "Units / LBS" rows = "1" cols="20"></textarea>
+                </form></td>
+        </tr>
+        <tr>
+            <th>Seller:</th>
+            <td><form action = "">
+                    <textarea name = "Seller" rows = "1" cols="20"></textarea>
+                </form></td>
+        </tr>
+        <tr>
+            <th>Order Date:</th>
+            <td><form action = "">
+                    <textarea name = "Order Date" rows = "1" cols="20"></textarea>
+                </form></td>
+        </tr>
+        <tr>
+            <th> Arrived:</th>
+            <td>  <form action = "">
+                    <select name = "answer"><option>No</option><option>Yes</option> </select></form></td>
+        </tr>
+    </table>
+    <button>Save</button>
 
-    <form action = "">
 
-            Units / LBS:
-            <textarea name = "Units / LBS" rows = "1" cols="20"></textarea>
-
-    </form>
-
-    <form action = "">
-
-            Seller:
-            <textarea name = "Seller" rows = "1" cols="20"></textarea>
-
-    </form>
-
-    <form action = "">
-
-            Order Date:
-            <textarea name = "Order Date" rows = "1" cols="20"></textarea>
-
-    </form>
-
-    <form action = "">
-    Arrived:
-        <select name = "answer"><option>No</option><option>Yes</option> </select></form>
-
-    <form action = "">
-        <select name = "editProduct">
-            <option>New Product</option>
-            <option>Organic Banana</option>
-            <option>Yummy Chickpeas</option>
-            <option>Cherry Tomatoes</option>
-            <option>Jimmy's Natural Potato Chips</option>
-        </select>
-    </form>
-
+</div>
 
 
 
