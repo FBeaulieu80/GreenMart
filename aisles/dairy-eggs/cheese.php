e9277d3ef22301650e95097893a910e8dc8246b6
Athigan1 <60296138+Athigan1@users.noreply.github.com>
2020-07-28 17:54:03 -0400

    Increments, selections, description toggle

--- aisles/dairy-eggs/cheese.php
+++ aisles/dairy-eggs/cheese.php
@@ -22,18 +22,63 @@
             <span class="sale-price">$3.29/lb</span><br />
             <br />
             <br />
+            <p style="color: darkslateblue;font-weight: bold;">Select between 3 types of cheese:
+                <select id="dlist" onChange="swapImage()">
+                    <option style="color: darkgreen;" value="../../images/dairy-eggs/cheese.jpg">Regular</option>
+                    <option style="color: crimson;" value="../../images/dairy-eggs/cheddar.jpg">Cheddar</option>
+                    <option style="color: blueviolet;" value="../../images/dairy-eggs/parmesan.jpg">Parmesan</option>
+                    </select></p>
+                    
+                    <script type="text/javascript">
+                    function swapImage(){
+                        var image = document.getElementById("imageToSwap");
+                        var dropd = document.getElementById("dlist");
+                        image.src = dropd.value;	
+                    };
+                    </script> <br>
+
             <div class="addtocart">
                 <form action="/action_page.php">
                     <label for="quantity">Quantity:</label> <br />
-                    <button type="submit" style="float:right">Add to Cart</button>
-                    <input type="text" id="quantity" name="quantity" value="1">
+                        <div onclick="increaseValue()" value="Increase Value">+</div>
+                            <input type="number" id="number" value="0" />
+                        <div onclick="decreaseValue()" value="Decrease Value">-</div><br>
+                      <script>
+                      function increaseValue() {
+                        var value = parseInt(document.getElementById('number').value, 10);
+                        value = isNaN(value) ? 0 : value;
+                        value++;
+                        document.getElementById('number').value = value;
+                      }
+                      function decreaseValue() {
+                        var value = parseInt(document.getElementById('number').value, 10);
+                        value = isNaN(value) ? 0 : value;
+                        value < 1 ? value = 1 : '';
+                        value--;
+                        document.getElementById('number').value = value;
+                      }
+                      </script>
+                    <button type="submit">Add to Cart</button>
+                    
                 </form>
             </div>
-            <p>Product of Switzerland.</p><br />
-            <h3>Description</h3>
-            <p>Cheese is a dairy product, derived from milk and produced in a wide range of flavors, textures and forms
+            <p>Product of Switzerland.</p>
+        
+            <script>
+                function ShowAndHide() {
+                    var x = document.getElementById('SectionName');
+                    if (x.style.display == 'none') {
+                        x.style.display = 'block';
+                    } else {
+                        x.style.display = 'none';
+                    }
+                }
+                </script>
+                <button onclick="ShowAndHide()">Detailed Description</button>
+          
+                <div id="SectionName" style="display:none"><p>Cheese is a dairy product, derived from milk and produced in a wide range of flavors, textures and forms
                 by coagulation of the milk protein casein. It comprises proteins and fat from milk, usually the milk of
-                cows, buffalo, goats, or sheep.</p>
+                cows, buffalo, goats, or sheep.</p></div>
         </div>
     </div>
 </div>
