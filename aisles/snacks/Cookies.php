b75d9309d44a91048789dee77f4fe7ffbcb5fe66
Laura Boivin <laurasuzanneboivin@gmail.com>
2020-07-30 14:04:52 -0400

    Working on cookies P3

--- aisles/snacks/Cookies.php
+++ aisles/snacks/Cookies.php
@@ -4,27 +4,34 @@
 <head>
     <meta name="author" content="Laura Boivin">
     <meta charset="UTF-8">
-    <title>Vegan Fairtrade Chocolate Cookies | Snacks</title>
+    <title>Vegan Fairtrade Cookies | Snacks | Green Mart</title>
     <link rel="stylesheet" type="text/css" href="../../css/main.css" />
     <link rel="stylesheet" type="text/css" href="../../css/p3.css" />
+
 </head>
-
-
 
 <body>
 <?php require_once "../../common/header.php"; ?>
 <div class="main">
+    <div class = "cookie-options">
 
+        <h3>Select one of our delicious cookie options</h3>
+        <button class = "cookie-button" id = "button1">Chocolate</button>
+        <button class = "cookie-button" id = "button2">Salted Caramel</button>
+        <button class = "cookie-button" id = "button3">Peanut Butter</button>
+
+    </div>
     <div class="grid-container">
         <div class="grid-item">
-            <img src="../../images/snacks/Chocolate%20cookies.jpg" alt="Vegan Fairtrade Chocolate Cookies"> <!-- Change -->
+            <img src="../../images/snacks/Chocolate%20cookies.jpg" alt="Chocolate Cookies"> <!-- default image -->
+            <script src = "../../js/cookies.js"></script>
         </div>
         <div class="grid-item">
-            <h2>Vegan Fairtrade Chocolate Cookies</h2> <!-- Change -->
-            <span class="price">$4.99/ea</span><br /> <!-- Change -->
+            <h2>Vegan Fairtrade Cookies</h2>
+            <span class="price">$4.99/ea</span><br />
 
             <br />
-            Product of Canada. <br /> <br /> <!-- Change -->
+            Product of Canada. <br /> <br />
             <div class="addtocart">
                 <form action="/action_page.php">
                     <label for="quantity">Quantity:</label> <br />
@@ -33,10 +40,11 @@
                 </form>
             </div>
             <br /> <br />
-            <h3>Description</h3> <!-- Change description below -->
-            <p>These vegan fairtrade chocolate cookies are a baker's masterpiece. This dessert is delicious, moist and melts
-            in your mouth. Most important of all, these cookies are ethical. Our cocoa beans are picked by farmers in Nigeria
-                who are well compensated for their work, as they should be.  </p>
+            <button class = "cookie-button" id = "button4">Description</button>
+            <script src = "../../js/cookies.js"></script>
+            <p>These vegan fairtrade cookies are a baker's masterpiece. This snack is delicious, moist and melts
+                in your mouth. All workers at every step of production
+                are well compensated for their work and all our ingredients are ethically sourced, as they should be.  </p>
         </div>
     </div>
 </div>
