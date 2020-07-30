c1e6908b2551d6fcdcecdc70e26cd245a253d419
Laura Boivin <laurasuzanneboivin@gmail.com>
2020-07-30 14:06:26 -0400

    Working on cookies P3

---
+++ js/cookies.js
@@ -0,0 +1,17 @@
+let img = document.querySelector(".grid-item img");
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
+
+button1.addEventListener("click", () => {
+    img.src = "../../images/snacks/Chocolate%20cookies.jpg"
+})
+
+button2.addEventListener("click", () => {
+    img.src = "../../images/snacks/peanut-butter-1164861_1920.jpg"
+})
+
+button3.addEventListener("click", () => {
+    img.src = "../../images/snacks/spice-643848_1920.jpg"
+})
+
