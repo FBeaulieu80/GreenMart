4c0dfe3a13885e5c3fded3a933c0146329fb3359
Laura Boivin <laurasuzanneboivin@gmail.com>
2020-08-02 00:01:00 -0400

    Changed images, delete button functional, check out button has an alert, continue shopping button implemented. Missing: increment/decrement & price change per item as well as global price adjustment.

--- p4.php
+++ p4.php
@@ -3,98 +3,120 @@
 
 <head>
     <meta name="author" content="Laura Boivin">
+    <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/p4.css">
+    <script src = "scripts/p4.js" async></script>
     <title>Shopping Cart (4) | Green Mart</title>
 </head>
 
-
 <body marginwidth="100%">
 <?php require "common/header.php"; ?>
+<div class="main">
+    <div class="grid-container">
+        <div class="grid-item">
+            <div class = "col">
 
-<button class = "continue">Continue Shopping</button>
-<h1 align="center" >My Shopping Cart</h1>
+                <form action = "aisles/index.php">
+                <button class = "continue" >Continue Shopping</button>
+                </form>
+
+                <h1 align="center" >My Shopping Cart</h1>
+
+
+
 <table align="center" cellspacing="20" >
-    <tr valign="top">
+
+        <tr valign="top" id = "checkout">
+            <th >
+
+                <br>
+                <button onclick = "checkOut()"> Checkout Now</button>
+                <h3> Order Summary</h3>
+
+                <span id = "orderTitle">Subtotal:</span>
+                <span id = "amount">$11.95</span><br>
+
+                <span id = "orderTitle">QST:</span>
+                <span id = "amount">$1.19</span><br>
+
+                <span id = "orderTitle">GST:</span>
+                <span id = "amount">$0.60</span><br><br>
+
+                <span id = "orderTitle">Total:</span>
+                <span id = "amount">$13.74</span><br><br>
+
+            </th>
+        </tr>
+
+    <tr valign="top" id = "itemBlock" >
         <th class = "img-with-text">
-            <h2>Carrot Cake</h2>
-            <img id = "food" src="images/bakery/carrot-cake.jpg"
-                 alt = "Carrot Cake">
-            <p>$11.99</p>
-            <button class = "add">+</button>
+
+            <h3>Carrot Cake</h3>
+            <img id = "food" src="images/bakery/carrot-cake.jpg" alt = "Carrot Cake"><br><br>
+            <span id = "subtotal"  > $11.99 </span><br><br>
+            <button id = "add" onclick = "addQuantity()">+</button>
             <form action = ""></form>
             <input type = "text" value = 1 id = "number" readonly>
-            <button class = "subtract">-</button><br/>
-            <button class = "delete">Remove Item </button>
+            <button id = "subtract" onclick = "subtractQuantity()">-</button><br/>
+            <button id = "delete" onclick ="parentElement.remove()">Remove Item </button><br><br>
+
         </th>
-        <th class = "checkout">   <button> Checkout Now</button>
-            <h3> Order Summary</h3>
-            <h4>4 Products </h4><br/>
-            <table>
-                <tr>
-                    <th>
-                        Subtotal:
-                    </th>
-                    <td>$11.95</td>
-                </tr>
-                <tr>
-                    <th>QST:</th>
-                    <td>$1.19</td>
-                </tr>
-                <tr>
-                    <th>GST:</th>
-                    <td>$0.60</td>
-                </tr>
-                <tr>
-                    <th>Total:</th>
-                    <td >$13.74</td>
-                </tr>
-            </table>
     </tr>
 
     <tr valign="top">
         <th class = "img-with-text">
-            <h2>Cheddar Cheese</h2>
-            <img id = "food" src="images/dairy-eggs/cheddar.jpg"
-                 alt = "Cheddar Cheese">
-            <p>$3.29</p>
-            <button class = "add">+</button>
+
+            <h3>Cheddar Cheese</h3>
+            <img id = "food" src="images/dairy-eggs/cheddar.jpg" alt = "Cheddar Cheese"><br><br>
+            <span id = "subtotal"  > $3.29 </span><br><br>
+            <button id = "add" onclick = "addQuantity()">+</button>
             <form action = ""></form>
             <input type = "text" value = 1 id = "number" readonly>
-            <button class = "subtract">-</button><br/>
-            <button class = "delete">Remove Item</button>
+            <button id = "subtract" onclick = "subtractQuantity()">-</button><br/>
+            <button id = "delete" onclick ="parentElement.remove()">Remove Item</button><br><br>
+
+        </th>
+    </tr>
+
+
+    <tr valign="top">
+        <th class="img-with-text">
+
+            <h3>Rainer Cherries</h3>
+            <img id = "food" src="images/fruit-veg/rainier-cherry.jpg" alt="Rainer Cherries"><br><br>
+            <span id = "subtotal"  > $4.99 </span><br><br>
+            <button id = "add" onclick = "addQuantity()">+</button>
+            <form action=""></form>
+            <input type = "text" value = 1 id = "number" readonly>
+            <button id = "subtract" onclick = "subtractQuantity()">-</button>
+            <br/>
+            <button id="delete" onclick ="parentElement.remove()">Remove Item</button><br><br>
         </th>
     </tr>
 
     <tr valign="top">
         <th class="img-with-text">
-            <h2>Rainer Cherries</h2>
-            <img id = "food" src="images/fruit-veg/rainier-cherry.jpg"
-                 alt="Rainer Cherries">
-            <p>$4.99</p>
-            <button class="add">+</button>
-            <form action=""></form>
-            <input type = "text" value = 1 id = "number" readonly>
-            <button class="subtract">-</button>
-            <br/>
-            <button class="delete">Remove Item</button>
-        </th>
 
-        <th class="img-with-text">
-            <h2>Roast Chicken</h2>
-            <img id = "food" src="images/meat/chicken.jpg"
-                 alt="Roast Chicken">
-            <p>$9.99</p>
-            <button class="add">+</button>
+            <h3>Roast Chicken</h3>
+            <img id = "food" src="images/meat/chicken.jpg" alt="Roast Chicken"><br><br>
+            <span id = "subtotal"  > $9.99 </span><br><br>
+            <button id = "add" onclick = "addQuantity()">+</button>
             <form action=""></form>
             <input type = "text" value = 1 id = "number" readonly>
-            <button class="subtract">-</button>
+            <button id = "subtract" onclick = "subtractQuantity()">-</button>
             <br/>
-            <button class="delete">Remove Item</button>
+            <button id="delete" onclick ="parentElement.remove()">Remove Item</button><br><br>
         </th>
     </tr>
+
 </table>
-<?php //require "common/footer.php"; ?>
-</body>
+            </div>
+        </div>
+    </div>
+</div>
 
+
+<?php require "../../common/footer.php"; ?>
+</body>
 </html>
---
+++ scripts/p4.js
@@ -0,0 +1,15 @@
+/*function subtractQuantity(){
+    if (document.getElementById("number").value > 0)
+    {document.getElementById("number").value--}
+
+    document.getElementById("subtotal").innerHTML = "$" + (document.getElementById("number").value * document.getElementById("subtotal")).toFixed(2);
+}
+
+function addQuantity(){
+    document.getElementById("number").value++
+    document.getElementById("subtotal").innerHTML = "$" + (document.getElementById("number").value * document.getElementById("subtotal")).toFixed(2);
+}*/
+
+function checkOut(){
+    alert("You have successfully completed your order. Your items will arrive in 2-3 business days. Thank you for shopping at Green Mart! See you again soon!");
+}
