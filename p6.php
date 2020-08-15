788255e22216ab578f8678051f5a9bff9932d7f9
Athigan1 <60296138+Athigan1@users.noreply.github.com>
2020-08-15 14:06:33 -0400

    Implemented form action and fixed input names

--- p6.php
+++ p6.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <link rel="shortcut icon" href="favicon.ico">
     <meta charset="UTF-8">
     <title>Sign Up | Green Mart</title>
@@ -12,45 +11,45 @@
 <body>
 <?php require_once "common/header.php" ?>
 <div class="main">
-    <form id="box">
-        <h4 style="font-weight: bold; color: darkblue; text-align: center;">SIGNUP TO CREATE A GREEN MART ACCOUNT</h4>
-        <h3 style="font-weight: bold; text-decoration: underline;">PROFILE</h3>
-        <label>
-            *FIRST NAME
-            <input type="text" name="firstname" id="firstname" required>
-        </label> <br><br>
-        <label>
-            *LAST NAME
-            <input type="text" name="lastname" id="lastname" required>
-        </label><br><br>
-        <label>
-            *ADDRESS
-            <input style="width: 70%;" type="text" name="address" id="address" required>
-        </label><br><br>
-        <label>
-            *POSTAL CODE
-            <input type="text" name="postalcode" id="postalcode" required>
-        </label><br><br>
-        <label>
-            *EMAIL
-            <input type="text" name="email" id="email" required>
-        </label><br><br>
-        <label>
-            PHONE NUMBER
-            <input type="text" name=phone id="phone">
-        </label><br><br>
-        <label>
-            PASSWORD (minimum 6 characters, including numbers and letters)<br>
-            <input type="password" value="" id="signup_password">
-        </label>
-        <label>
-            <input type="checkbox" onclick="myFunction()" required>
-            Show Password
-        </label><br><br>
-        <label>
-            CONFIRM PASSWORD<br>
-            <input type="password" value="" id="signup_confirm_password">
-        </label><br><br>
+    <form id="box" action="/GreenMartUserAccounts.php" target="_blank" method="post">
+            <h4 style="font-weight: bold; color: darkblue; text-align: center;">SIGNUP TO CREATE A GREEN MART ACCOUNT</h4>
+            <h3 style="font-weight: bold; text-decoration: underline;">PROFILE</h3>
+            <label>
+                FIRST NAME
+                <input type="text" name="firstName" required>
+            </label> <br><br>
+            <label>
+                MIDDLE NAME
+                <input type="text" name="middleName">
+            </label><br><br>
+            <label>
+                LAST NAME
+                <input type="text" name="lastName" required>
+            </label><br><br>
+            <label>
+                EMAIL
+                <input type="text" name="email" required>
+            </label><br><br>
+            <label>
+                FULL ADDRESS
+                <input style="width: 70%;" type="text" name="fullAddress" required>
+            </label><br><br>
+            <label>
+                PHONE NUMBER
+                <input type="text" name="phoneNumber">
+            </label><br><br>
+            <label>
+                PASSWORD (minimum 6 characters)<br>
+                <input type="password" name="password" value="" id="signup_password">
+            </label>
+            <label>
+                <input type="checkbox" onclick="myFunction()" required>
+                Show Password
+            </label><br><br>
+            <label>
+                CONFIRM PASSWORD<br>
+                <input type="password" value="" id="signup_confirm_password">
+            </label><br><br>
         <script>
             function myFunction() {
                 const x = document.getElementById("signup_password");
@@ -62,14 +61,11 @@
                 }
             }
         </script>
-
-
-        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-        <label for="vehicle1"> Subscribe to receive email notifications on Green Mart promotions. Unsubscribe at any
+        <input type="checkbox" name="label">
+        <label for="label"> Subscribe to receive email notifications on Green Mart promotions. Unsubscribe at any
             time.</label><br><br>
-        <button type="button" class="submit" style="font-weight: bold;">FINISH PROFILE</button>
+        <button type="submit" name="submit" id="submit" style="font-weight: bold;">FINISH PROFILE</button>
     </form>
-
     <p style="text-align: center;">Already have a Green Mart account? <a href="p5.php">Login</a> to shop online.</p>
 </div>
 </body>
